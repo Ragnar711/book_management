6a06a3c7aedf8034f121896de868654d7093a090
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2023-08-03 12:01:05 +0100

    Login page rebrand

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import style from "./Login.module.css";
 
 const Login = () => {
+    const navigate = useNavigate();
+
     const [showForgotPasswordModal, setShowForgotPasswordModal] =
         useState(false);
 
@@ -18,29 +21,69 @@ const Login = () => {
             <form className={style.form}>
                 <h2>Login</h2>
                 <div className={style.group}>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id={style.username} required />
+                    <input
+                        required=""
+                        type="text"
+                        className={style.input}
+                        placeholder="Username"
+                    />
+                    <span className={style.highlight}></span>
+                    <span className={style.bar}></span>
                 </div>
                 <div className={style.group}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id={style.password} required />
+                    <input
+                        required=""
+                        type="text"
+                        className={style.input}
+                        placeholder="Password"
+                    />
+                    <span className={style.highlight}></span>
+                    <span className={style.bar}></span>
                 </div>
                 <div className={style.buttonGroup}>
-                    <button type="submit" className={style.login}>
-                        Login
+                    <button
+                        className={style.learn_more}
+                        onClick={() => {
+                            navigate("/Home");
+                        }}
+                    >
+                        <span className={style.circle} aria-hidden="true">
+                            <span
+                                className={`${style.icon} ${style.arrow}`}
+                            ></span>
+                        </span>
+                        <span className={style.button_text}>Login</span>
                     </button>
                 </div>
-                <button type="button" className={style.registerButton}>
-                    Register
+                <button className={style.learn_more}>
+                    <span className={style.circle} aria-hidden="true">
+                        <span className={`${style.icon} ${style.arrow}`}></span>
+                    </span>
+                    <span className={style.button_text}>Register</span>
                 </button>
                 <button
                     type="button"
-                    className={style.forgotLink}
+                    className={style.cta}
                     onClick={handleForgotPasswordClick}
                 >
-                    Forgot Password
+                    <span className={style.hover_underline_animation}>
+                        Forgot Password
+                    </span>
+                    <svg
+                        viewBox="0 0 46 16"
+                        height="10"
+                        width="30"
+                        xmlns="http://www.w3.org/2000/svg"
+                        id="arrow-horizontal"
+                    >
+                        <path
+                            transform="translate(30)"
+                            d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
+                            data-name="Path 10"
+                            id="Path_10"
+                        ></path>
+                    </svg>
                 </button>
-
                 {showForgotPasswordModal && (
                     <div className={style.modalOverlay}>
                         <div className={style.modalContent}>
